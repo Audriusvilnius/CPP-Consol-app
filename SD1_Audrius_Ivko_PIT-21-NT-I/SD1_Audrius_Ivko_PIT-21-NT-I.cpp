3bcad037a2dc3d90e29408132725e8da41239c29
Audrius Ivko <101082768+Audriusvilnius@users.noreply.github.com>
2022-10-16 11:42:53 +0300

    Add cin

--- SD1_Audrius_Ivko_PIT-21-NT-I/SD1_Audrius_Ivko_PIT-21-NT-I.cpp
+++ SD1_Audrius_Ivko_PIT-21-NT-I/SD1_Audrius_Ivko_PIT-21-NT-I.cpp
@@ -27,60 +27,65 @@ class student {
 private:
     string id_name;
     string id_surname;
-    int nd1;
-    int nd2;
-    int nd3;
-    int exam;
+    double nd1;
+    double nd2;
+    double nd3;
+    double exam;
     bool nd_passed();
 public:
-    void set_data(string, string, int, int, int, int);
+    
+    student(): id_surname("no surname"), id_name("no name"), nd1(0), nd2(0), nd3(0), exam(0) {}//numatytasis konstruktorius
+
+    void set_data(string, string, double, double, double, double);
     void print();
-    int final_grade();
-    void getgrade() {
-        cin >> id_name;
-        cin >> id_surname;
-        cin >> nd1;
-        cin >> nd2;
-        cin >> nd3;
-        cin >> exam;
-    }
-    void gradeprint() {
-        cout << id_name << "/" << id_surname << "/" << nd1 << "/" << nd2 << "/" << nd3 << "/" << exam;
-
-    }
-
+    double final_grade();
+      
 };
 
-
-
-
+  
 int main() {
+    int input_type = 0;
+    string id_name, id_surname;
+    double nd1, nd2, nd3, exam;
+    student stud, stud1, sutudinput;
+
+    cout << "Duomenys ivedimas bus atliekamas is domenu failo spauskit 1, duomenis ivedami is klaviaturos spauskite 2: "; cin >> input_type;
     
-    
-    student stud, stud1, stud3;
-       
-        stud.set_data("Audrius", "Ivko", 9, 10, 7, 8);
-        stud1.set_data("Audrius", "Ivko", 7, 8, 9, 10);
-        stud.print(); 
+
+    if (input_type == 1) {
+cout << "Pavarde" << setw(7) << "Vardas" << setw(7) << "Galotini (Vid.)" << endl;
+        stud.set_data("Pavardenis ", "Vaderins", 9.8, 10, 7, 8);
+        stud1.set_data("Pavardenis ", "Vaderins", 7, 8, 9, 10);
+        stud.print();
         stud1.print();
-        stud3.getgrade();
-        stud3.gradeprint();
-       
     }
-   
-
-
-
-void student::set_data(string p1, string p2, int p3, int p4, int p5, int p6) {
-    id_name = p1;
-    id_surname = p2;
+    if (input_type == 2) {
+        
+        cout << "Iveskite studento varda: "; cin >> id_name;
+        cout << "Iveskite studento pavarde: "; cin >> id_surname;
+        cout << "Iveskite atsiskatymo ND1 ivertinima: "; cin >> nd1;
+        cout << "Iveskite atsiskatymo ND2 ivertinima: "; cin >> nd2;
+        cout << "Iveskite atsiskatymo ND3 ivertinima: "; cin >> nd3;
+        cout << "Iveskite ekzamino ivertinima: "; cin >> exam;
+cout << "Pavarde" << setw(7) << "Vardas" << setw(7) << "Galotini (Vid.)" << endl;
+        sutudinput.set_data(id_surname, id_name, nd1, nd2, nd3, exam);
+        sutudinput.print();
+        //cin.get();
+    }
+ 
+ };
+    
+     
+void student::set_data(string p1, string p2, double p3, double p4, double p5, double p6) {
+    id_name = p2;
+    id_surname = p1;
 
     if (p3 >= 0 && p3 <= 10) {
         nd1 = p3;
     }
     else {
         nd1 = 0;
-        cout << "nd1 grade is incorrect!" << endl;
+        cout << "ND1 neteisingas ivertis!" << endl;
 
     }
     if (p4 >= 0 && p4 <= 10) {
@@ -88,7 +93,7 @@ void student::set_data(string p1, string p2, int p3, int p4, int p5, int p6) {
     }
     else {
         nd2 = 0;
-        cout << "nd2 grade is incorrect!" << endl;
+        cout << "ND2 neteisingas ivertis!" << endl;
 
     }
     if (p5 >= 0 && p5 <= 10) {
@@ -96,7 +101,7 @@ void student::set_data(string p1, string p2, int p3, int p4, int p5, int p6) {
     }
     else {
         nd3 = 0;
-        cout << "nd3 grade is incorrect!" << endl;
+        cout << "ND3 neteisingas ivertis!" << endl;
 
     }
     if (p6 >= 0 && p6 <= 10) {
@@ -104,15 +109,16 @@ void student::set_data(string p1, string p2, int p3, int p4, int p5, int p6) {
     }
     else {
         exam = 0;
-        cout << "Exam grade is incorrect!" << endl;
+        cout << "Egzamino ivertis neteisingas!" << endl;
 
     }
 }
 void student::print() {
-    cout << id_name << "\t| " << id_surname << "\t|";
+   
+    cout << id_surname  << "\t| " << id_name << "\t|";
 
     if (nd1 == 0) cout << "N/A" << "\t| ";
-    else cout << nd1 << "\t| ";
+    else cout << fixed << setprecision(2) << nd1 << "\t| ";
 
     if (nd2 == 0) cout << "N/A" << "\t| ";
     else cout << nd2 << "\t| ";
@@ -123,7 +129,7 @@ void student::print() {
     if (exam == 0) cout << "N/A" << "\t| ";
     else cout << exam << "\t| ";
 
-    int fg = final_grade();
+    double fg = final_grade();
     if (fg == 0) cout << "N/A";
     else cout << fg;
 
@@ -139,12 +145,12 @@ bool student::nd_passed() {
     else return 0;
 }
 
-int student::final_grade() {
+double student::final_grade() {
     if ((nd_passed() ) && (exam > 4)) {
-        return round((nd1 + nd2 + nd3) / 3.0 * 0.4 + exam * 0.6);
+        return ((nd1 + nd2 + nd3) / 3.0 * 0.4 + exam * 0.6);
     }
     else {
-        cout << "Final grade cannot be formed!" << endl;
+        cout << "Galutinis rezultatas nefirmuojamas" << endl;
         return 0;
     }
 }
